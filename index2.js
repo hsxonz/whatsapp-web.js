fa53345ace8861528a4d1344fba8492e7fafca2e
hsxonz <heroallstart2001@gmail.com>
2024-01-16 03:19:35 +0700

    First Ininit

---
+++ index2.js
@@ -0,0 +1,38 @@
+const { Client, LocalAuth } = require('./index');
+
+const client = new Client({
+    authStrategy: new LocalAuth(),
+    // proxyAuthentication: { username: 'username', password: 'password' },
+    puppeteer: { 
+        // args: ['--proxy-server=proxy-server-that-requires-authentication.example.com'],
+        headless: false
+    }
+});
+
+client.initialize();
+
+client.on('loading_screen', (percent, message) => {
+    console.log('LOADING SCREEN', percent, message);
+});
+
+client.on('qr', (qr) => {
+    // NOTE: This event will not be fired if a session is specified.
+    console.log('QR RECEIVED', qr);
+});
+
+client.on('authenticated', () => {
+    console.log('AUTHENTICATED');
+});
+
+client.on('auth_failure', msg => {
+    // Fired if session restore was unsuccessful
+    console.error('AUTHENTICATION FAILURE', msg);
+});
+
+client.on('ready', () => {
+    console.log('READY');
+});
+
+client.on('message', async msg => {
+    console.log('MESSAGE RECEIVED', msg);
+});
